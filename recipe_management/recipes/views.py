64e052686edc870cc057ba75c6c546f126b4a09b
Ajowi254 <ajowi.bery@gmail.com>
2024-05-18 15:05:01 +0300

    SEEDED DATA

--- recipe_management/recipes/views.py
+++ recipe_management/recipes/views.py
@@ -3,9 +3,17 @@ from django.views import generic
 from .models import Recipe
 from django.urls import reverse_lazy
 
+
 class RecipeListView(generic.ListView):
     model = Recipe
     template_name = 'recipes/recipe_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.GET.get('search', '')
+        if search_term:
+            queryset = queryset.filter(title__icontains=search_term)
+        return queryset
 
 class RecipeDetailView(generic.DetailView):
     model = Recipe
